90e1a5444cc2f2f78f6de8bb775bc495152cd089
GanievAhmadsher <ganievahmadsher6205@gmail.com>
2024-12-23 13:39:09 +0500

    github

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // * Work with GitHub in Node.js ------------------------------ start --
 
-console.log("Welcome to Branch: `github`");
-console.log("Branch Answer: `hello hithub`");
 console.log("Welcome to Branch: `main`");
 console.log("Branch Answer: `hello main`");
+console.log("Welcome to Branch: `github`");
+console.log("Branch Answer: `hello hithub`");
 
 // * Work with GitHub in Node.js ------------------------------  end  --
